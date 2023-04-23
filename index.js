d2d2fe9ecdc25ac4fff823006f5b66cb6f641801
Arghya <arghyadutta080@gmail.com>
2023-04-23 12:00:41 +0530

    cors update

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ require('dotenv').config();
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors());
 
 app.use('/user', userRoute);
 app.use('/task', taskRoute);
