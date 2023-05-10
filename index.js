eaddfa08c4b78b04a3454e869929a6a0ace5a91a
Arghya <arghyadutta080@gmail.com>
2023-05-10 20:29:14 +0530

    frontendend port changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:3000',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true
 }));
